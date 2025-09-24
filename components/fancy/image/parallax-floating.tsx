383b6588c7990538a6c982c63d928818bda1c7e2
cynthia <cynthia_cui@sfu.ca>
2025-09-24 14:13:48 -0700

    staging fix

--- components/fancy/image/parallax-floating.tsx
+++ components/fancy/image/parallax-floating.tsx
@@ -45,7 +45,7 @@ const Floating = ({
       }
     >()
   )
-  const mousePositionRef = useMousePositionRef(containerRef)
+  const mousePositionRef = useMousePositionRef(containerRef as React.RefObject<HTMLElement>)
 
   const registerElement = useCallback(
     (id: string, element: HTMLDivElement, depth: number) => {
