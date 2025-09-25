2928c019b1ccad21fc1b7ee9f5b393882bc3e938
cynthia <cynthia_cui@sfu.ca>
2025-09-24 19:01:00 -0700

    image logic fixed

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -134,7 +134,7 @@ export default function WorkPage() {
             {filteredProjects.map((project) => (
               <WorkCard
                 key={project.id}
-                image={project.image[1] || project.image[0]}
+                image={project.image.length === 1 ? project.image[0] : project.image[1]}
                 title={project.title}
                 onClick={() => handleProjectClick(project)}
                 opacity={project.opacity}
